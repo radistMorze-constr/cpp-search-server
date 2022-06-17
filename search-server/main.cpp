b4e3e33f03783cef334b80d7488f3d735af0ae3b
unknown <ladinove@gmail.com>
2022-06-17 21:33:50 +0300

    second sprint v2

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
@@ -163,10 +164,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
         return rating_sum / static_cast<int>(ratings.size());
     }
 
@@ -345,11 +343,26 @@ void TestExcludeStopWordsFromAddedDocumentContent() {
 // Поддержка минус-слов
 void TestExcludeDocumentsWithMinusWords() {
     const int doc_id = 42;
+    const string content1 = "cat in the city"s;
+    const string content2 = "cat in the town"s;
+    const vector<int> ratings = { 1, 2, 3 };
+    SearchServer server1, server2;
+    server1.AddDocument(doc_id, content1, DocumentStatus::ACTUAL, ratings);
+    ASSERT(server1.FindTopDocuments("cat -city"s).empty());
+    server2.AddDocument(doc_id, content2, DocumentStatus::ACTUAL, ratings);
+    const auto found_docs = server2.FindTopDocuments("cat -city"s);
+    ASSERT_EQUAL_HINT(found_docs[0].id, doc_id, "The document without minus-word isn't found"s);
+}
+
+//Проверка на добавление документов в server
+void TestSuprplaceDocument() {
+    const int doc_id = 42;
     const string content = "cat in the city"s;
     const vector<int> ratings = { 1, 2, 3 };
     SearchServer server;
+    ASSERT_EQUAL(server.GetDocumentCount(), 0);
     server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-    ASSERT(server.FindTopDocuments("cat -city"s).empty());
+    ASSERT_EQUAL(server.GetDocumentCount(), 1);
 }
 
 // Матчинг документов
@@ -358,54 +371,68 @@ void TestMatchDocuments() {
     const string content = "cat in the city which is placed on the other side"s;
     const vector<int> ratings = { 1, 2, 3 };
     SearchServer server;
+    server.SetStopWords("in the is on which");
     server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-    //Проверим что вернуться все совпадения
-    auto matched_document = server.MatchDocument("cat placed side home", 42);
-    vector<string> matched_words = get<0>(matched_document);
-    vector<string> right_result = { "cat"s, "placed"s, "side"s };
-    ASSERT_EQUAL(matched_words, right_result);
-    //Проверим, что при содержании минус-слов вернется пустйо список
-    matched_document = server.MatchDocument("-cat placed side home", 42);
-    matched_words = get<0>(matched_document);
-    ASSERT(matched_words.empty());
+    {
+        //Проверим что вернуться все совпадения, кроме стоп-слов
+        auto matched_document = server.MatchDocument("cat is placed in the side home"s, 42);
+        const auto [matched_words, doc_status] = matched_document;
+        vector<string> right_result = { "cat"s, "placed"s, "side"s };
+        ASSERT_EQUAL(matched_words, right_result);
+        ASSERT(doc_status == DocumentStatus::ACTUAL);
+    }
+
+    {
+        //Проверим, что при содержании минус-слов вернется пустой список
+        auto matched_document = server.MatchDocument("-cat placed side home"s, 42);
+        const auto [matched_words, doc_status] = matched_document;
+        ASSERT(matched_words.empty());
+        ASSERT(doc_status == DocumentStatus::ACTUAL);
+    }
 }
 
 //Сортировака по релевантности
 void TestSortByRelevation() {
     const string content1 = "cat in the city which is placed on the other side"s;
+    double relevance1 = log(1)/11;
     const string content2 = "cat played in the garage"s;
+    double relevance2 = 0.2 * log(1) + 0.2 * log(3);
     const string content3 = "cat vs dog who win"s;
+    double relevance3 = 0.2 * log(1) + 2 * 0.2 * log(3);
     const vector<int> ratings = { 1, 2, 3 };
     SearchServer server;
     server.AddDocument(1, content1, DocumentStatus::ACTUAL, ratings);
     server.AddDocument(2, content2, DocumentStatus::ACTUAL, ratings);
     server.AddDocument(3, content3, DocumentStatus::ACTUAL, ratings);
-    const auto found_docs = server.FindTopDocuments("cat played who win");
-    ASSERT_EQUAL(found_docs[0].id, 3);
-    ASSERT_EQUAL(found_docs[1].id, 2);
-    ASSERT_EQUAL(found_docs[2].id, 1);
+    const auto found_docs = server.FindTopDocuments("cat played who win"s);
+    ASSERT_EQUAL(found_docs[0].relevance, relevance3);
+    ASSERT(found_docs[1].relevance - relevance2 < EQUAL_MAX_DIFFERENCE);
+    ASSERT(found_docs[2].relevance - relevance1 < EQUAL_MAX_DIFFERENCE);
 }
 
 //Корректно считается рейтинг
 void TestRating() {
     const string content = "cat vs dog who win"s;
-    const vector<int> ratings1 = { 1, 2, 3 }; //rating = 2
-    const vector<int> ratings2 = { 2, 3 }; //rating = 2
-    const vector<int> ratings3 = { 0 }; //rating = 0
-    const vector<int> ratings4 = { }; //rating = 0
+    const vector<int> ratings1 = { 1, 2, 3 };
+    const int rating1 = (1 + 2 + 3) / 3;
+    const vector<int> ratings2 = { 2, 3 };
+    const int rating2 = (2 + 3) / 2;
+    const vector<int> ratings3 = { 0 };
+    const vector<int> ratings4 = { };
+    const int rating_zero = 0;
     SearchServer server1, server2, server3, server4;
     server1.AddDocument(1, content, DocumentStatus::ACTUAL, ratings1);
-    const auto found_docs1 = server1.FindTopDocuments("cat");
-    ASSERT_EQUAL(found_docs1[0].rating, 2);
+    const auto found_docs1 = server1.FindTopDocuments("cat"s);
+    ASSERT_EQUAL(found_docs1[0].rating, rating1);
     server2.AddDocument(2, content, DocumentStatus::ACTUAL, ratings2);
-    const auto found_docs2 = server2.FindTopDocuments("cat");
-    ASSERT_EQUAL(found_docs2[0].rating, 2);
+    const auto found_docs2 = server2.FindTopDocuments("cat"s);
+    ASSERT_EQUAL(found_docs2[0].rating, rating2);
     server3.AddDocument(3, content, DocumentStatus::ACTUAL, ratings3);
-    const auto found_docs3 = server3.FindTopDocuments("cat");
-    ASSERT_EQUAL(found_docs3[0].rating, 0);
+    const auto found_docs3 = server3.FindTopDocuments("cat"s);
+    ASSERT_EQUAL(found_docs3[0].rating, rating_zero);
     server4.AddDocument(4, content, DocumentStatus::ACTUAL, ratings4);
-    const auto found_docs4 = server4.FindTopDocuments("cat");
-    ASSERT_EQUAL(found_docs4[0].rating, 0);
+    const auto found_docs4 = server4.FindTopDocuments("cat"s);
+    ASSERT_EQUAL(found_docs4[0].rating, rating_zero);
 
 }
 
@@ -419,11 +446,11 @@ void TestFilterPredicate() {
     server.AddDocument(2, content, DocumentStatus::ACTUAL, ratings1);
     server.AddDocument(3, content, DocumentStatus::BANNED, ratings2);
     server.AddDocument(1, content, DocumentStatus::IRRELEVANT, ratings3);
-    auto found_docs = server.FindTopDocuments("cat", [](int id, DocumentStatus status, int rating) {
+    auto found_docs = server.FindTopDocuments("cat"s, [](int id, DocumentStatus status, int rating) {
         return id == 2;
         });
     ASSERT_EQUAL(found_docs[0].id, 2);
-    found_docs = server.FindTopDocuments("cat", [](int id, DocumentStatus status, int rating) {
+    found_docs = server.FindTopDocuments("cat"s, [](int id, DocumentStatus status, int rating) {
         return rating == 2;
         });
     ASSERT_EQUAL(found_docs[0].id, 2);
@@ -438,36 +465,44 @@ void TestSearchDocumentsByStatus() {
     server.AddDocument(2, content, DocumentStatus::BANNED, ratings);
     server.AddDocument(3, content, DocumentStatus::IRRELEVANT, ratings);
     server.AddDocument(4, content, DocumentStatus::REMOVED, ratings);
-    auto found_docs = server.FindTopDocuments("cat", DocumentStatus::ACTUAL);
+    auto found_docs = server.FindTopDocuments("cat"s, DocumentStatus::ACTUAL);
     ASSERT_EQUAL(found_docs[0].id, 1);
-    found_docs = server.FindTopDocuments("cat", DocumentStatus::BANNED);
+    found_docs = server.FindTopDocuments("cat"s, DocumentStatus::BANNED);
     ASSERT_EQUAL(found_docs[0].id, 2);
-    found_docs = server.FindTopDocuments("cat", DocumentStatus::IRRELEVANT);
+    found_docs = server.FindTopDocuments("cat"s, DocumentStatus::IRRELEVANT);
     ASSERT_EQUAL(found_docs[0].id, 3);
-    found_docs = server.FindTopDocuments("cat", DocumentStatus::REMOVED);
+    found_docs = server.FindTopDocuments("cat"s, DocumentStatus::REMOVED);
     ASSERT_EQUAL(found_docs[0].id, 4);
+    //Проверка на поиск документа с несуществующим статусом
+    found_docs = server.FindTopDocuments("cat"s, static_cast<DocumentStatus>(10));
+    ASSERT(found_docs.empty());
 }
 
 //Правильно считает реливантность
 void TestCorrectCalculationRelevation() {
     const string content1 = "белый кот и модный ошейник"s; //tf-idf = 0.1014
+    double relevance1 = 0.25 * log(1.5);
     const string content2 = "пушистый кот пушистый хвост"s; //tf-idf = 0.6507
+    const double relevance2 = 0.5 * log(3) + 0.25 * log(1.5);
     const string content3 = "ухоженный пёс выразительные глаза"s; //tf-idf = 0.2746
+    const double relevance3 = 0.25 * log(3);
     const vector<int> ratings = { };
     SearchServer server;
     server.AddDocument(1, content1, DocumentStatus::ACTUAL, ratings);
     server.AddDocument(2, content2, DocumentStatus::ACTUAL, ratings);
     server.AddDocument(3, content3, DocumentStatus::ACTUAL, ratings);
-    const auto found_docs = server.FindTopDocuments("пушистый ухоженный кот");
-    ASSERT_EQUAL(found_docs[0].id, 2);
-    ASSERT_EQUAL(found_docs[1].id, 3);
-    ASSERT_EQUAL(found_docs[2].id, 1);
+    //Документы отсортированы в порядке убывания relevance: 2, 3, 1
+    const auto found_docs = server.FindTopDocuments("пушистый ухоженный кот"s);
+    ASSERT(found_docs[0].relevance - relevance2 < EQUAL_MAX_DIFFERENCE);
+    ASSERT(found_docs[1].relevance - relevance3 < EQUAL_MAX_DIFFERENCE);
+    ASSERT(found_docs[2].relevance - relevance1 < EQUAL_MAX_DIFFERENCE);
 }
 
 // Функция TestSearchServer является точкой входа для запуска тестов
 void TestSearchServer() {
     RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
     RUN_TEST(TestExcludeDocumentsWithMinusWords);
+    RUN_TEST(TestSuprplaceDocument);
     RUN_TEST(TestMatchDocuments);
     RUN_TEST(TestSortByRelevation);
     RUN_TEST(TestRating);
