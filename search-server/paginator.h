9176ea0e4ca6d7e492b94d94365994dad9ee5d72
unknown <ladinove@gmail.com>
2022-07-03 13:25:06 +0300

    fourth sprint v4

--- search-server/paginator.h
+++ search-server/paginator.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cassert>
 
 template <typename Iterator>
 class IteratorRange {
@@ -41,8 +42,8 @@ template <typename Iterator>
 class Paginator {
 public:
     Paginator(Iterator begin, Iterator end, size_t page_size) {
+        assert(end >= begin && page_size > 0);
         for (size_t left = distance(begin, end); left > 0;) {
-            //const size_t current_page_size = min(page_size, left); 
             const size_t current_page_size = (page_size < left) ? page_size : left;
             const Iterator current_page_end = next(begin, current_page_size);
             pages_.push_back({ begin, current_page_end });
--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -6,14 +6,10 @@ class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
     // сделаем "обертки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
-    template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        AddRequest(result.size());
-        return result;
-    }
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
     int GetNoResultRequests() const;
 private:
     struct QueryResult {
@@ -28,3 +24,10 @@ private:
 
     void AddRequest(int results_num);
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    AddRequest(result.size());
+    return result;
+}
